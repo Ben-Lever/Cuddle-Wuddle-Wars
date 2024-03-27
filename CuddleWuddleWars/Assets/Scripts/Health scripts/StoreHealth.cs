ca4cae9e8f8f160f59f9b7ae3b7917ea04484d2d
Ben-Lever <1026824@student.sae.edu.au>
2024-03-27 13:22:26 +0800

    tried some balancing

--- CuddleWuddleWars/Assets/Scripts/Health scripts/StoreHealth.cs
+++ CuddleWuddleWars/Assets/Scripts/Health scripts/StoreHealth.cs
@@ -9,14 +9,38 @@ public class StoreHealth : MonoBehaviour
 
     public GameObject gameOverCanvas;
 
+
+    public GameObject life1;
+    public GameObject life2;
+    public GameObject life3;
+
     void Start()
     {
         currentHealth = maxHealth;
+        life1.SetActive(true);
+        life2.SetActive(true);
+        life3.SetActive(true);
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+
+        if (currentHealth <= 2)
+        {
+            life3.SetActive(false);
+        }
+        else if (currentHealth <= 1)
+        {
+            life3.SetActive(false);
+            life2.SetActive(false);
+        }
+        else if (currentHealth <= 0)
+        {
+            life3.SetActive(false);
+            life2.SetActive(false);
+            life1.SetActive(false);
+        }
 
         if (currentHealth < 0)
         {//anything you want to happen when they lose here
